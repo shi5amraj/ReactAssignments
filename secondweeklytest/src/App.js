1d371277842f1ce0147a0d95ff834fb76ca84798
shi5amraj <shivamsinghtomar07@gmail.com>
2023-03-21 15:28:30 +0530

    second weekly test

---
+++ secondweeklytest/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './Components/Board';
+
+
+function App() {
+  return (
+    <div className="App">
+
+   
+    <Board/>
+ 
+
+      
+    </div>
+  );
+}
+
+export default App;
